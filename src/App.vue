102945b3f69ec944d75d8477d3a805af214d149f
Sargis Margaryan <sargismargaryan0605@gmail.com>
2024-10-21 20:05:23 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+import Navbar from './components/Navbar.vue';
+import { RouterView } from 'vue-router';
+</script>
+<template>
+  <Navbar />
+  <RouterView />
+</template>
